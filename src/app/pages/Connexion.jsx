c6314b5f3ff8c170713a6b39809d522ab7a30e66
Mbarike <thiamhawa361@gmail.com>
2026-06-17 16:45:11 +0000

    commit

--- src/app/pages/Connexion.jsx
+++ src/app/pages/Connexion.jsx
@@ -1,13 +1,10 @@
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify';
-
-const URL_FRONT = import.meta.env.VITE_URL_FRONT;
-
+const URL_front = import.meta.env.VITE_URL_FRONT;
 
 
 const Connexion = () => {
-
   const [ email , setEmail ] = useState('');
   const [ password , setPassword ] = useState('');
   const navigate = useNavigate();
