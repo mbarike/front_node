436b22be8683365beec017a6a765be6d6b11ca81
Mbarike <thiamhawa361@gmail.com>
2026-06-17 16:45:23 -0300

    commit

--- src/app/pages/Connexion.jsx
+++ src/app/pages/Connexion.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify';
-const URL_front = import.meta.env.VITE_URL_FRONT;
+ import.meta.env.VITE_URL_FRONT;
 
 
 const Connexion = () => {
